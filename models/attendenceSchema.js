5afaee20387a0e3ecd07198cc8e2271bf712fa5a
pragati patidar <140271307+1pragati@users.noreply.github.com>
2024-08-03 13:57:23 +0530

    Add files via upload

---
+++ models/attendenceSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const AttendanceSchema = new mongoose.Schema({
+  employeId: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    enum: ['Present', 'Absent', 'Halfday'],
+    default: 'Absent',
+  },
+});
+
+module.exports = mongoose.model('Attendance', AttendanceSchema);
